01e02b7e55a659c116567dde88c5049110d62c14
kofia894 <kofia894@gmail.com>
2021-05-07 20:49:48 +0000

    added social icons

--- views/creator-details.php
+++ views/creator-details.php
@@ -153,7 +153,7 @@
 
                       </div>
 
-                      <div class="add-button btn btn-outline-danger" > 
+                      <div class="delete-button btn btn-outline-danger" > 
                       <svg xmlns="http://www.w3.org/2000/svg" width="40" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -164,10 +164,9 @@
 
              
 
-                  <div class="form-group mb-3" style="background-color: transparent;">
+                  <div class="form-group mb-3 d-flex justify-content-center" style="background-color: transparent;">
                     <input type="file" id="picture" onchange="pictureInput(event)" style="display: none;" name="file" />
-                    <input id="pic-btn" type="button" class="form-control btn btn-primary rounded submit " 
-                      value="Select Profile Picture..."/>
+                    <input id="pic-btn" type="button" class=" btn btn-primary rounded submit " value="Select Profile Picture"/>
                   </div>
                   
                   <div class="d-flex justify-content-center">
@@ -249,8 +248,13 @@
               <div class="social">
                 <a href=""><i class="fa fa-twitter"></i></a>
                 <a href=""><i class="fa fa-facebook"></i></a>
-                <a href=""><i class="fa fa-google-plus"></i></a>
                 <a href=""><i class="fa fa-linkedin"></i></a>
+                <a href=""> <i class="fa fa-youtube"></i></a>
+                <a href=""> <i class="fa fa-twitch"></i></a>
+                <a href=""> <i class="fa fa-facebook"></i></a>
+                <a href=""> <i class="fa fa-desktop"></i></a>
+                <a href=""> <i class="fa fa-desktop"></i></a>
+               
               </div>
               <p>Voluptatem perferendis sed assumenda voluptatibus. Laudantium molestiae sint. Doloremque odio dolore dolore sit. Quae labore alias ea omnis ex expedita sapiente molestias atque. Optio voluptas et.</p>
 
